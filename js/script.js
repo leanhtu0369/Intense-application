145d4c7f8781db0d3cf173a15933b258c3711f17
LeTu <leanhtu0369@gmail.com>
2020-09-04 15:56:41 +0700

    edit scroll

--- js/script.js
+++ js/script.js
@@ -1,17 +1,6 @@
 $(document).ready(function(){
   var featuresTop = $("#features").offset().top;
   checkPoint = featuresTop - 200;
-
-  // scroll top
-  window.onscroll = function() {scrollFunction()};
-  function scrollFunction() {
-    if ( document.body.scrollTop > checkPoint || document.documentElement.scrollTop > checkPoint ) {
-      $("#scroll-top").addClass("active");
-    } 
-    else {
-      $("#scroll-top").removeClass("active");
-    }
-  }
   
   jQuery("#scroll-top").on("click", function (e) {
     e.preventDefault();
@@ -20,39 +9,51 @@ $(document).ready(function(){
 
 
   arrMenu = ["home", "features", "responsive", "help", "services", "testimonials"];
+  arrMenuTop = [];
+  nthChildMenu = [];
   for (let index = 0; index < arrMenu.length; index++) {
     const menuItem = arrMenu[index];
-    // console.log(menuItem);
+    var menuItemTop = $("#" + menuItem).offset().top - 56;
+    arrMenuTop.push(menuItemTop);
+    var nthChildMenuItem = index + 1;
+    nthChildMenu.push(nthChildMenuItem);
+  }
+  arrMenuTop.push(Infinity);
 
-    var idmenuItem = "#" + menuItem;
-    // console.log(idmenuItem);
+  // scroll top
+  window.onscroll = function() {scrollFunction()};
 
-    var menuItemTop = menuItem + "Top";
-    // console.log(menuItemTop);
-
-    var menuItemTop = $(idmenuItem).offset().top;
-    // console.log(menuItemTop);
-
-    var menuItemHeight = $(idmenuItem).height();
-    // console.log(menuItemHeight);
-
-    var  menuItemBottom = menuItemTop + menuItemHeight;
-    // console.log(menuItemBottom);
-    
-    var nthChildMenu = index + 1;
-
-    if( menuItemTop < $(this).scrollTop() < menuItemBottom){
+  function activeMenu(a,b) {
+    if(($(this).scrollTop() > arrMenuTop[a]) && $(this).scrollTop() < arrMenuTop[b] ) {
       $('.header__menu__nav-item').removeClass('active');
-      $(".header__menu__nav-item:nth-child(" + nthChildMenu + ")").addClass('active');
-      console.log(nthChildMenu);
+      $(".header__menu__nav-item:nth-child(" + nthChildMenu[a] + ")").addClass('active');
     }
   }
 
+  function scrollFunction() {
+    if ( document.body.scrollTop > checkPoint || document.documentElement.scrollTop > checkPoint ) {
+      $("#scroll-top").addClass("active");
+    } 
+    else {
+      $("#scroll-top").removeClass("active");
+    }
 
+    for (let index = 0; index < nthChildMenu.length; index++) {
+      activeMenu(index, index+1);
+    }
+  }
+
+  nthChildMenu.forEach( function (element, index) {
+    $(".header__menu__nav-item:nth-child("+ element +") a").on("click", function (e) {
+      e.preventDefault();
+      $('html, body').animate({
+        scrollTop: arrMenuTop[index] 
+      }, 500);
+    });
+  });
 });
 
 
 function tuToogleMenu(t) {
   jQuery(t).closest("header").toggleClass("active");
 }
-
